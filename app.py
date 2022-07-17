c2b74ef97e506d29896b0b66c1c98ce61dd6a6a1
bosscoder global <bosscoder1325@gmail.com>
2022-07-17 17:33:15 +0530

    dude

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pyrebase
 import json
 
@@ -15,6 +15,10 @@ config = {
     "measurementId": "G-3GT9ED7KXE"
 }
 
+session = False
+print("global session id", id(session))
+cid = ""
+
 # config = {
 #     "apiKey": "AIzaSyCc0veJ4Kez3iLS8U-qjxhOMFwnzA7WZ7U",
 #     "authDomain": "fir-46336.firebaseapp.com",
@@ -28,42 +32,165 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-data1 = db.child("data").get().val()
-data2 = db.child("student_info").get().val()
-attend = json.loads(json.dumps(data1))
-student_info = json.loads(json.dumps(data2))
+# data1 = db.child("data").get().val()
+# data2 = db.child("student_info").get().val()
+# attend = json.loads(json.dumps(data1))
+# student_info = json.loads(json.dumps(data2))
+
+
+@app.route('/edit_student', methods=['POST', 'GET'])
+def edit_student():
+    if request.method == 'POST':
+        current_data = request.form.get('data').replace(
+            "(", "").replace(")", "").split(',')
+        current_data = [i.replace("'", "").strip() for i in current_data]
+        current_keys = ('usn', 'name', 'sem', 'branch', 's1', 's2', 'date')
+        current_dic = {}
+        print("this is data got", current_data)
+        print(len(current_data), len(current_keys))
+        for i in range(len(current_data)):
+            current_dic.update({current_keys[i]: current_data[i]})
+        print("curretn dic", current_dic)
+
+    return render_template('edit_student.html', data=current_dic)
+    # return render_template('edit_student.html')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def fetch_data():
-    branch = sem = date = ""
-    att_data = []
-    headings = ('USN', 'NAME', 'SEM', 'BRANCH','SESSION 1', 'SESSION 2', 'DATE')
-    if request.method == "POST":
-        # getting input with name = fname in HTML form
-        branch = request.form.get("branch")
-        sem = request.form.get("sem")
-        date = request.form.get("date")
+    if session:
+        data1 = db.child("data").get().val()
+        data2 = db.child("student_info").get().val()
+        attend = json.loads(json.dumps(data1))
+        student_info = json.loads(json.dumps(data2))
 
-        print(branch, sem, date)
+        branch = sem = date = ""
+        att_data = []
+        headings = ('USN', 'NAME', 'SEM', 'BRANCH',
+                    'SESSION 1', 'SESSION 2', 'DATE', 'EDIT', 'DELETE')
+        if request.method == "POST":
+            # getting input with name = fname in HTML form
+            branch = request.form.get("branch")
+            sem = request.form.get("sem")
+            date = request.form.get("date")
 
-        
+            print(branch, sem, date)
 
-        fd = attend[branch][sem][date]
-        fd_keys = fd.keys()
-        for j in fd_keys:
-            temp = []
-            temp.append(j)
-            temp.append(student_info[branch][sem][j]["name"])
-            temp.append(sem)
-            temp.append(branch)
-            temp.append(fd[j]['s1'])
-            temp.append(fd[j]['s2'])
-            temp.append(date)
-            att_data.append(tuple(temp))
+            fd = attend[branch][sem][date]
+            fd_keys = fd.keys()
+            for j in fd_keys:
+                temp = []
+                temp.append(j)
+                temp.append(student_info[branch][sem][j]["name"])
+                temp.append(sem)
+                temp.append(branch)
+                temp.append(fd[j]['s1'])
+                temp.append(fd[j]['s2'])
+                temp.append(date)
+                att_data.append(tuple(temp))
 
-    return render_template('index.html', headings = headings , rows = att_data)
+        return render_template('index.html', headings=headings, rows=att_data)
+    else:
+        return render_template('login.html')
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    data = db.child("creds").get().val()
+    creds = json.loads(json.dumps(data))
+    if request.method == 'POST':
+        cid = request.form.get('uname')
+        passw = request.form.get('pass')
+        if cid in creds.keys():
+            if passw == creds[cid]:
+                global session
+                session = True
+                print("internal session id", id(session))
+                print("session started", session)
+                return redirect('/')
+            else:
+                print("check your creds")
+        else:
+            print('user not found')
+    else:
+        print("not working")
+    return render_template('login.html')
+
+
+@app.route('/edited', methods=['POST', 'GET'])
+def edited():
+    data1 = db.child("data").get().val()
+    data2 = db.child("student_info").get().val()
+    attend = json.loads(json.dumps(data1))
+    student_info = json.loads(json.dumps(data2))
+
+    headings = ('USN', 'NAME', 'SEM', 'BRANCH',
+                'SESSION 1', 'SESSION 2', 'DATE', 'EDIT', 'DELETE')
+    usn = name = sem = branch = s1 = s2 = ""
+    if request.method == 'POST':
+        all_data = request.form.get("all_data")
+
+        data = [i.strip() for i in all_data.strip().replace(
+            "{", "").replace("}", "").replace("'", "").split(",")]
+
+        for i in data:
+            if i.startswith('branch'):
+                temp = i.strip().split(":")
+                branch = temp[1].strip()
+            elif i.startswith('sem'):
+                temp = i.strip().split(":")
+                sem = temp[1].strip()
+            elif i.startswith('usn'):
+                temp = i.strip().split(":")
+                usn = temp[1].strip()
+            elif i.startswith('date'):
+                temp = i.strip().split(":")
+                date = temp[1].strip()
+
+            s1 = request.form.get('s1')
+            s2 = request.form.get('s2')
+
+    # all_data_dic me ye mila
+    # {'usn': '1kt18cs004', 'name': 'Diwan', 'sem': 'sem5',
+    #     'branch': 'cse', 's1': 'True', 's2': 'True', 'date': '16-7-2022'}
+
+    attend[branch][sem][date][usn]['s1'] = s1
+    attend[branch][sem][date][usn]['s2'] = s2
+
+    db.child('data').update(attend)
+    print(usn, name, sem, branch)
+    return redirect('/')
+
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete_data():
+    data1 = db.child("data").get().val()
+    data2 = db.child("student_info").get().val()
+    attend = json.loads(json.dumps(data1))
+    student_info = json.loads(json.dumps(data2))
+
+    current_data = request.form.get('data').replace(
+        "(", "").replace(")", "").split(',')
+    current_data = [i.replace("'", "").strip() for i in current_data]
+    current_keys = ('usn', 'name', 'sem', 'branch', 's1', 's2', 'date')
+    current_dic = {}
+    print("this is data got", current_data)
+    print(len(current_data), len(current_keys))
+    for i in range(len(current_data)):
+        current_dic.update({current_keys[i]: current_data[i]})
+    print("curretn dic", current_dic)
+
+    # all_data_dic me ye mila
+    # {'usn': '1kt18cs004', 'name': 'Diwan', 'sem': 'sem5',
+    #     'branch': 'cse', 's1': 'True', 's2': 'True', 'date': '16-7-2022'}
+    print(type(current_dic))
+    branch = current_dic["branch"]
+    sem = current_dic["sem"]
+    date = current_dic["date"]
+    usn = current_dic["usn"]
+    del attend[branch][sem][date][usn]
+    db.child('data').update(attend)
+    return redirect('/')
 
 # @app.route('/')
 # # ‘/’ URL is bound with hello_world() function.
@@ -73,4 +200,5 @@ def fetch_data():
 
 # main driver function
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host='0.0.0.0', port=50000, debug=True)
--- funny.py
+++ funny.py
@@ -1,35 +1,81 @@
+from flask import Flask, render_template, request, redirect
+import pyrebase
 import json
+config = {
+    "apiKey": "AIzaSyAZoAADcMtseOtxeAh6jLcpH3or24ZhL8c",
+    "authDomain": "rfid-aaea6.firebaseapp.com",
+    "databaseURL": "https://rfid-aaea6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "rfid-aaea6",
+    "storageBucket": "rfid-aaea6.appspot.com",
+    "messagingSenderId": "621829893183",
+    "appId": "1:621829893183:web:e04119660c08c0c44a1c8a",
+    "measurementId": "G-3GT9ED7KXE"
+}
+# from sklearn.cluster import k_means
 
-from sklearn.cluster import k_means
-
-f = open('temp_data_file.json')
-data = json.load(f)
-maindata = data['data']
-studinfo = data['student_info']
-# print(type(data))
+# f = open('temp_data_file.json')
+# data = json.load(f)
+# maindata = data['data']
+# studinfo = data['student_info']
+# # print(type(data))
 
 
-branch = "cse"
-sem = "sem5"
-date = "16-7-2022"
-# session = "session1"
+# branch = "cse"
+# sem = "sem5"
+# date = "16-7-2022"
+# # session = "session1"
 
-# headings = ('USN','NAME','SEM','BRANCH','SESSION','DATE')
+# # headings = ('USN','NAME','SEM','BRANCH','SESSION','DATE')
 
-att_data = []
+# att_data = []
 
 
-fd = maindata[branch][sem][date]
-fd_keys = fd.keys()
-for j in fd_keys:
-    temp = []
-    temp.append(j)
-    temp.append(studinfo[branch][sem][j]["name"])
-    temp.append(sem)
-    temp.append(branch)
-    temp.append(fd[j]['s1'])
-    temp.append(fd[j]['s2'])
-    temp.append(date)
-    att_data.append(tuple(temp))
+# fd = maindata[branch][sem][date]
+# fd_keys = fd.keys()
+# for j in fd_keys:
+#     temp = []
+#     temp.append(j)
+#     temp.append(studinfo[branch][sem][j]["name"])
+#     temp.append(sem)
+#     temp.append(branch)
+#     temp.append(fd[j]['s1'])
+#     temp.append(fd[j]['s2'])
+#     temp.append(date)
+# #     att_data.append(tuple(temp))
 
-print(att_data)
+# data = ('1kt18cs004', 'Diwan', 'sem5', 'cse', False, True, '16-7-2022')
+# current_keys = ('usn', 'name', 'sem', 'branch', 's1', 's2', 'date')
+# current_dic = {}
+# for i in range(len(current_data)):
+#     current_dic.update({current_keys[i]: current_data[i]})
+
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
+# data1 = db.child("data").get().val()
+# data2 = db.child("student_info").get().val()
+# attend = json.loads(json.dumps(data1))
+# student_info = json.loads(json.dumps(data2))
+# print(attend)
+# usn = '1kt18cs004'
+# # name = request.form.get('name')
+# sem = 'sem5'
+# branch = 'cse'
+# s1 = True
+# s2 = True
+# date = '16-7-2022'
+
+# attend[branch][sem][date][usn]['s1'] = s1
+# attend[branch][sem][date][usn]['s2'] = s2
+# db.child('data').update(attend)
+
+
+data = [i.strip() for i in "{'usn': '1kt18cs004', 'name': 'Diwan', 'sem': 'sem5', 'branch': 'cse', 's1': 'True', 's2': 'True', 'date': '16-7-2022'}".strip().replace("{","").replace("}", "").replace("'", "").split(",")]
+
+# data = [i.split(":") for i in data]
+
+for i in data:
+    if i.startswith('branch'):
+        temp = i.strip().split(":")
+        print(temp[1])
+
+# print(data)
